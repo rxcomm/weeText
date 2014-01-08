364bb5a537edc5e999fab2fbd114c7b650e28211
David R. Andersen <k0rx@RXcomm.net>
2014-01-08 06:51:02 -0600

    repackage

--- weetext.py
+++ weetext.py
@@ -141,7 +141,7 @@ def sentCB(buf_name, command, return_code, out, err):
     if return_code == weechat.WEECHAT_HOOK_PROCESS_ERROR:
         weechat.prnt("", "Error with command '%s'" % command)
         return weechat.WEECHAT_RC_OK
-    if return_code >= 0:
+    if return_code > 0:
         weechat.prnt("", "return_code = %d" % return_code)
     if out != "":
         weechat.prnt(weechat.buffer_search('python', buf_name), out)
@@ -221,6 +221,124 @@ if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, 
     passwd = weechat.config_get_plugin('passwd')
     if re.search('sec.*data', passwd):
         passwd=weechat.string_eval_expression(passwd, {}, {}, {})
+
+    # write the helper files if needed
+    if not os.path.isfile(weechat_dir + '/python/wtrecv.py'):
+        with open(weechat_dir + '/python/wtrecv.py', 'w') as f:
+            f.write("""#!/usr/bin/env python
+
+import sys
+import cPickle
+import time
+import re
+import os
+import glob
+from googlevoice import Voice
+from googlevoice.util import input
+from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup, SoupStrainer
+
+user_path = os.path.expanduser('~')
+
+class Conversation(object):
+    def __init__(self, conv_id, number, messages):
+        self.conv_id = conv_id
+        self.number = number
+        self.messages = messages
+
+    def new_messages(self, other):
+        assert len(self.messages) <= len(other.messages)
+        return other.messages[len(self.messages):]
+
+    def __iter__(self):
+        return iter(reversed(self.messages))
+
+class SMS:
+
+    def getsms(self):
+        # We could call voice.sms() directly, but I found this does a rather
+        # inefficient parse of things which pegs a CPU core and takes ~50 CPU
+        # seconds, while this takes no time at all.
+        data = voice.sms.datafunc()
+        data = re.search(r'<html><\!\[CDATA\[([^\]]*)', data, re.DOTALL).groups()[0]
+
+        divs = SoupStrainer(['div', 'input'])
+        tree = BeautifulSoup(data, parseOnlyThese=divs)
+
+        convos = []
+        conversations = tree.findAll("div", attrs={"id" : True},recursive=False)
+        for conversation in conversations:
+            inputs = SoupStrainer('input')
+            tree_inp = BeautifulSoup(str(conversation),parseOnlyThese=inputs)
+            phone = tree_inp.find('input', "gc-quickcall-ac")['value']
+
+            smses = []
+            msgs = conversation.findAll(attrs={"class" : "gc-message-sms-row"})
+            for row in msgs:
+                msgitem = {"id" : conversation["id"]}
+                spans = row.findAll("span", attrs={"class" : True}, recursive=False)
+                for span in spans:
+                    cl = span["class"].replace('gc-message-sms-', '')
+                    msgitem[cl] = (" ".join(span.findAll(text=True))).strip()
+                if msgitem["text"]:
+                    msgitem["text"] = BeautifulStoneSoup(msgitem["text"],
+                                      convertEntities=BeautifulStoneSoup.HTML_ENTITIES
+                                      ).contents[0]
+                    msgitem['phone'] = phone
+                    smses.append(msgitem)
+            convos.append(Conversation(conversation['id'], phone, smses))
+        print cPickle.dumps(convos)
+
+if __name__ == '__main__':
+
+    email = sys.argv[1]
+    passwd = sys.argv[2]
+    poll_interval = sys.argv[3]
+
+    time.sleep(float(poll_interval))
+
+    # create voice instance if no texts are being sent
+    while True:
+        f = glob.glob(user_path + '/.weechat/.gvlock*')
+        if f == []:
+            voice = Voice()
+            voice.login(email=email, passwd=passwd)
+            sms = SMS()
+            sms.getsms()
+            break
+        else:
+            time.sleep(1)
+""")
+    os.chmod(weechat_dir + '/python/wtrecv.py', 0755)
+
+    if not os.path.isfile(weechat_dir + '/python/wtsend.py'):
+        with open(weechat_dir + '/python/wtsend.py', 'w') as f:
+            f.write("""#!/usr/bin/env python
+
+import sys
+import os
+from googlevoice import Voice
+from googlevoice.util import input
+
+user_path = os.path.expanduser('~')
+email = sys.argv[1]
+passwd = sys.argv[2]
+number = sys.argv[3]
+payload = sys.argv[4]
+msg_id = sys.argv[5]
+
+open(user_path + '/.weechat/.gvlock.' + msg_id, 'a').close()
+
+try:
+    voice = Voice()
+    voice.login(email, passwd)
+    voice.send_sms(number, payload)
+    print '<message sent>'
+except:
+    print '<message NOT sent!>'
+
+os.remove(user_path + '/.weechat/.gvlock.' + sys.argv[5])
+""")
+    os.chmod(weechat_dir + '/python/wtsend.py', 0755)
 
     # register the hooks
     weechat.hook_signal("buffer_switch","update_encryption_status","")
